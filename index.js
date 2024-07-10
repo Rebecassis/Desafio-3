e2f3e774a96245cce34848676d278906a4c578a0
Rebecassis <Rebecassis06@gmail.com>
2024-07-10 13:53:32 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+class Herói{
+    constructor (nome, idade, tipo){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+    atacar() {
+        let ataque;
+        switch (this.tipo) {
+            case 'mago':
+                ataque = 'explosion';
+                break;
+            case 'guerreiro':
+                ataque = 'espada'
+                break;
+            case 'monge' : 
+                ataque = 'artes marciais'
+                break;
+            case 'ninja' : 
+                ataque = 'shuriken';
+                break;
+            default:
+                ataque = 'realizou um último ataque';     
+        }
+        console.log(`O ${this.tipo} Megumim atacou usando ${ataque}`;)
+    }
+}
+const heroi1 = new Heroi ('Megumim', 22, 'mago'); 
+heroi1.atacar(); 
+console.log()
